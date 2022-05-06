f0db33dd1e27815912b1e1879401f92c86d2575c
Alexsmik <alexsmik@yandex.ru>
2022-05-06 18:14:18 +0300

    uptodate

---
+++ src/Components/timeline/timelineElements.js
@@ -0,0 +1,150 @@
+import silvero from '../../images/portfolioimg/silvero.jpg';
+import avto from '../../images/portfolioimg/avto.jpg';
+import marfin from '../../images/portfolioimg/marfin.jpg';
+import ekom from '../../images/portfolioimg/ekom.jpg';
+import feniks from '../../images/portfolioimg/feniks.jpg';
+import sp from '../../images/portfolioimg/sp.jpg';
+import mebrest from '../../images/portfolioimg/mebrest.jpg';
+import sverch from '../../images/portfolioimg/sverch.jpg';
+import vorota from '../../images/portfolioimg/vorota.jpg';
+import poligraf1 from '../../images/portfolioimg/poligraf1.jpg';
+import poligraf4 from '../../images/portfolioimg/poligraf4.jpg';
+import poligraf6 from '../../images/portfolioimg/poligraf6.jpg';
+
+let timelineElements = [
+  {
+    id: 1,
+    image: marfin,
+    link: "",
+    title: "Отель \"Марфино\"",
+    description:
+        "Дизайн, Разработка, Полиграфия, СЕО продвижение",
+    buttonText: "https://www.marfinohotel.ru/",
+    date: "2016 - 2019",
+    icon: "web",
+  },
+  {
+    id: 2,
+    image: sverch,
+    link: "",
+    title: "Отель \"Сверчков\"",
+    description:
+        "Дизайн, Разработка, Админ панель, СЕО продвижение",
+    buttonText: "Company Website",
+    date: "2012 - 2018",
+    icon: "web",
+  },
+  {
+    id: 3,
+    image: sp,
+    link: "",
+    title: "У.К.С. г. Сергиев Посад",
+    description:
+        "Дизайн, Разработка, Полиграфия, СЕО продвижение",
+    buttonText: "http://uks-sp.ru/",
+    date: "2014 - 2018",
+    icon: "git",
+  },
+  {
+    id: 4,
+    image: avto,
+    link: "",
+    title: "Автосервис \"Автотун\"",
+    description:
+      "Дизайн, Разработка, Полиграфия, СЕО продвижение",
+    buttonText: " ",
+    date: "2010 - 2014",
+    icon: "web",
+  },
+  {
+    id: 5,
+    image: silvero,
+    link: "",
+    title: "Ювелирная мастерская \"Сильверо\"",
+    description:
+      "Дизайн, Разработка, Полиграфия, СЕО продвижение",
+    buttonText: " ",
+    date: "2010 - 2018",
+    icon: "web",
+  },
+  {
+    id: 6,
+    image: ekom,
+    link: "ekom",
+    title: "ГК \"Строй Спектр\"",
+    description:
+      "Дизайн, Разработка, Полиграфия, СЕО продвижение",
+    buttonText: " ",
+    date: "20010 - 2014",
+    icon: "school",
+  },
+  {
+    id: 7,
+    image: feniks,
+    link: "",
+    title: "Промышленная Безопасность \"Феникс\"",
+    description:
+      "Дизайн, Разработка, Полиграфия, СЕО продвижение",
+    buttonText: "College Projects",
+    date: "2008 - 2012",
+    icon: "school",
+  },
+  {
+    id: 8,
+    image: mebrest,
+    link: "",
+    title: "Мастерская по реставрации мебели \"Мебрест\"",
+    description:
+        "Дизайн, Разработка, Полиграфия, СЕО продвижение ",
+    buttonText: " ",
+    date: "2010 - 2014",
+    icon: "school",
+  },
+  {
+    id: 9,
+    image: vorota,
+    link: "",
+    title: "Продажа откатных ворот",
+    description:
+        "Дизайн, Разработка, СЕО продвижение ",
+    buttonText: " ",
+    date: "2008 - 2010",
+    icon: "school",
+  },
+  {
+    id: 10,
+    image: poligraf1,
+    link: "",
+    title: "Полиграфия \"Буклеты\"",
+    description:
+        "Дизайн, Верстка",
+    buttonText: "College Projects",
+    date: "2006 - 2010",
+    icon: "school",
+  },
+  {
+    id: 11,
+    image: poligraf4,
+    link: "",
+    title: "Полиграфия \"Этикетки\"",
+    description:
+        "Дизайн, Верстка ",
+    buttonText: "College Projects",
+    date: "2006 - 2010",
+    icon: "school",
+  },
+  {
+    id: 12,
+    image: poligraf6,
+    link: "",
+    title: "Полиграфия \"Плакаты\"",
+    description:
+        "Дизайн, Верстка ",
+    buttonText: " ",
+    date: "2006 - 2010",
+    icon: "school",
+  },
+
+];
+
+export default timelineElements;
