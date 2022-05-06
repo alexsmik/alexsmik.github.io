164c524581b888b243348b0066940983789012f1
Alexsmik <alexsmik@yandex.ru>
2022-05-06 19:13:53 +0300

    some things

--- src/Components/resume/UI/index.js
+++
@@ -1,8 +0,0 @@
-// UI components:
-// controls:
-export { default as Avatar } from './Avatar'
-export { default as Range } from './Range'
-
-// layouts:
-export { default as Title } from './Title'
-export { default as Descr } from './Descr'
